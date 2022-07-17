f56521b80cae85040196446832b5b66872ff3b4a
Charles Russell <cj.russell2396@gmail.com>
2022-07-17 16:14:27 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,4 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function () {
     loader.style.display = "none";
 });
-console.log("Hello World");
+console.log("Hello there! If you are reading this you are awesome :)");
